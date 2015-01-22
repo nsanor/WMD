a19326af10ffbfafc4756b2598f882a5b64e5422
nsanor <nsanor@gmail.com>
2015-01-21 22:21:37 -0500

    Added map to map tab.

--- app/src/main/java/com/example/sdp11/wmd/MapFragment.java
+++ app/src/main/java/com/example/sdp11/wmd/MapFragment.java
@@ -7,6 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,8 +35,42 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        // Gets the MapView from the XML layout and creates it
+        MapView mapView = (MapView) view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        GoogleMap googleMap = mapView.getMap();
+
+        // latitude and longitude
+        double latitude = 41.13747;
+        double longitude = -81.47430700000001;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Home Yo!");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(13).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        return view;
     }
-
-
 }
---
+++ app/src/main/java/com/example/sdp11/wmd/ThrowDataPoint.java
@@ -0,0 +1,8 @@
+package com.example.sdp11.wmd;
+
+/**
+ * Created by Noah on 1/21/2015.
+ */
+public class ThrowDataPoint {
+    private int _id;
+}
