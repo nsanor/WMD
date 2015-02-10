c7b1b420d469ba1f6d54a1efb1ab1bfefa4fb415
nsanor <nsanor@gmail.com>
2015-02-10 09:35:55 -0500

    Added throw adapter class for custom rows in the data tab list view.

---
+++ app/src/main/java/com/example/sdp11/wmd/ThrowAdapter.java
@@ -0,0 +1,49 @@
+package com.example.sdp11.wmd;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nsanor on 2/10/2015.
+ */
+public class ThrowAdapter extends ArrayAdapter<CalculatedThrowData> {
+    private ArrayList<CalculatedThrowData> items;
+    private Context context;
+
+    public ThrowAdapter(Context context, int textViewResourceId, ArrayList<CalculatedThrowData> items) {
+        super(context, textViewResourceId, items);
+        this.items = items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.row, null);
+        }
+        CalculatedThrowData ctd = items.get(position);
+        if (ctd != null) {
+            TextView id = (TextView) v.findViewById(R.id.throw_id);
+            TextView dist = (TextView) v.findViewById(R.id.total_distance);
+            TextView tintegrity = (TextView) v.findViewById(R.id.throw_integrity);
+            if (id != null) {
+                id.setText(String.valueOf(ctd.getThrowId()));
+            }
+            if(dist != null){
+                dist.setText(String.valueOf(ctd.getTotalDistance()));
+            }
+            if(tintegrity != null){
+                dist.setText(String.valueOf(ctd.getThrowIntegrity()));
+            }
+        }
+        return v;
+    }
+
+}
