541dd166785b74f6c5a936b028a211f284c4d7fa
nsanor <nsanor@gmail.com>
2015-02-10 09:35:20 -0500

    Added classes for general GPS and IMU data.

---
+++ app/src/main/java/com/example/sdp11/wmd/GPSDataPoint.java
@@ -0,0 +1,8 @@
+package com.example.sdp11.wmd;
+
+/**
+ * Created by nsanor on 2/10/2015.
+ */
+public class GPSDataPoint {
+    //Implement using throw id, lat, long, time, etc.
+}
---
+++ app/src/main/java/com/example/sdp11/wmd/IMUDataPoint.java
@@ -0,0 +1,8 @@
+package com.example.sdp11.wmd;
+
+/**
+ * Created by nsanor on 2/10/2015.
+ */
+public class IMUDataPoint {
+    //Implement using throw id, and 9 necessary parameters
+}
