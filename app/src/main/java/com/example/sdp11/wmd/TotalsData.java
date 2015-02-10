7e34f0951abaf422d0997affff844c7568f9deba
nsanor <nsanor@gmail.com>
2015-02-10 09:46:45 -0500

    Added totals data to hold global data.

---
+++ app/src/main/java/com/example/sdp11/wmd/TotalsData.java
@@ -0,0 +1,10 @@
+package com.example.sdp11.wmd;
+
+/**
+ * Created by nsanor on 2/10/2015.
+ */
+public class TotalsData {
+    //Average distance, angle, sync counter to differentiate throws, etc.
+    //Used as a global variable.
+    //Create all as static variables.
+}
