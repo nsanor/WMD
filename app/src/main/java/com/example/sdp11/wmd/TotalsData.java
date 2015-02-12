67dd22984ea9f0f1d5d583e1e2677b46585513b9
nsanor <nsanor@gmail.com>
2015-02-11 23:48:43 -0500

    Added some basic attributes with getters and setters.

--- app/src/main/java/com/example/sdp11/wmd/TotalsData.java
+++ app/src/main/java/com/example/sdp11/wmd/TotalsData.java
@@ -7,4 +7,31 @@ public class TotalsData {
     //Average distance, angle, sync counter to differentiate throws, etc.
     //Used as a global variable.
     //Create all as static variables.
+    static private double averageDistance;
+    static private double averageAngle;
+    static private int syncCount;
+
+    public static double getAverageDistance() {
+        return averageDistance;
+    }
+
+    public static void setAverageDistance(double averageDistance) {
+        TotalsData.averageDistance = averageDistance;
+    }
+
+    public static double getAverageAngle() {
+        return averageAngle;
+    }
+
+    public static void setAverageAngle(double averageAngle) {
+        TotalsData.averageAngle = averageAngle;
+    }
+
+    public static int getSyncCount() {
+        return syncCount;
+    }
+
+    public static void setSyncCount(int syncCount) {
+        TotalsData.syncCount = syncCount;
+    }
 }
