87a75beaa93521ea8f792762d1d2613b5e3e6efc
nsanor <nsanor@gmail.com>
2015-02-20 12:43:05 -0500

    Added bluetooth service to connect and sync in the background. Connected service to the connect tab. Need to test this with the device.

--- app/src/main/java/com/example/sdp11/wmd/ConnectFragment.java
+++ app/src/main/java/com/example/sdp11/wmd/ConnectFragment.java
@@ -4,40 +4,50 @@ package com.example.sdp11.wmd;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.Handler;
-import android.support.v4.content.LocalBroadcastManager;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConnectFragment extends Fragment{
+    private final static String TAG = ConnectFragment.class.getSimpleName();
 
-    private Button on, off, search, paired;
+    private Button button_toggle, button_search;
     View view;
+
+    private boolean mConnected = false;
+
+    private BluetoothLeService mBluetoothLeService;
+
+    private String mDeviceName;
+    private String mDeviceAddress;
 
     private BluetoothAdapter BA;
     private ListView lv;
@@ -46,6 +56,7 @@ public class ConnectFragment extends Fragment{
     private List values;
 
     private BluetoothGatt mBluetoothGatt;
+    private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics;
 
     private boolean mScanning;
     private Handler mHandler;
@@ -53,8 +64,40 @@ public class ConnectFragment extends Fragment{
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 1000;
 
+    private int mConnectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+//
+//
+//    public final static String ACTION_GATT_CONNECTED =
+//            "com.example.sdp11.wmd.ACTION_GATT_CONNECTED";
+//    public final static String ACTION_GATT_DISCONNECTED =
+//            "com.example.sdp11.wmd.ACTION_GATT_DISCONNECTED";
+//    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+//            "com.example.sdp11.wmd.ACTION_GATT_SERVICES_DISCOVERED";
+//    public final static String ACTION_DATA_AVAILABLE =
+//            "com.example.sdp11.wmd.ACTION_DATA_AVAILABLE";
+//    public final static String EXTRA_DATA =
+//            "com.example.sdp11.wmd.EXTRA_DATA";
+//
+    private final String LIST_NAME = "NAME";
+    private final String LIST_UUID = "UUID";
+
+
     public ConnectFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBluetoothLeService = new BluetoothLeService();
+        //getActivity().bindService(mBluetoothLeService);
+        Intent gattServiceIntent = new Intent(getActivity(), BluetoothLeService.class);
+        getActivity().bindService(gattServiceIntent, mServiceConnection, getActivity().BIND_AUTO_CREATE);
     }
 
 
@@ -66,19 +109,19 @@ public class ConnectFragment extends Fragment{
                 (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
         BA = bluetoothManager.getAdapter();
 
-        on = (Button)view.findViewById(R.id.Toggle);
-        on.setOnClickListener(new View.OnClickListener() {
+        button_toggle = (Button)view.findViewById(R.id.Toggle);
+        button_toggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toggle(view);
             }
         });
 
-        paired = (Button)view.findViewById(R.id.Paired);
-        paired.setOnClickListener(new View.OnClickListener() {
+        button_search = (Button)view.findViewById(R.id.Paired);
+        button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(BA.getState() == BluetoothAdapter.STATE_ON) scanLeDevice(true);
+                if (BA.getState() == BluetoothAdapter.STATE_ON) scanLeDevice(true);
             }
         });
 
@@ -93,12 +136,54 @@ public class ConnectFragment extends Fragment{
             public void onItemClick(AdapterView<?> aView, View v, int position, long id) {
                 //Log.e(String.valueOf(position), "Connect here.");
                 BluetoothDevice device = listAdapter.getDevice(position);
-                mBluetoothGatt = device.connectGatt(getActivity(), false, mGattCallback);
+                mBluetoothGatt = device.connectGatt(getActivity(), false, mBluetoothLeService.getGattCallback());
             }
         });
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e("", "Unable to initialize Bluetooth");
+                getActivity().finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
 
     private void scanLeDevice(final boolean enable) {
         if (enable) {
@@ -137,45 +222,13 @@ public class ConnectFragment extends Fragment{
         }
     };
 
-    private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            //Connection established
-            if (status == BluetoothGatt.GATT_SUCCESS
-                    && newState == BluetoothProfile.STATE_CONNECTED) {
-
-                //Discover services
-                gatt.discoverServices();
-
-            } else if (status == BluetoothGatt.GATT_SUCCESS
-                    && newState == BluetoothProfile.STATE_DISCONNECTED) {
-
-                //Handle a disconnect event
-
-
-            }
-        }
-    };
-//
-//        @Override
-//        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-//            if (status == BluetoothGatt.GATT_SUCCESS) {
-//
-//                Log.i("ConnectFragment", "Connected to: " + gatt.getDevice());
-//            }
-//        }
-//    };
-
-
-
-
     public void toggle(View view){
         if(!BA.isEnabled()){
             //Intent TurnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             //startActivityForResult(TurnOn, 0);
             BA.enable();
             while (BA.getState() != BluetoothAdapter.STATE_ON);
-            Toast.makeText(getActivity(), "Bluetooth is now on!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Bluetooth is now button_toggle!", Toast.LENGTH_SHORT).show();
         }
         else {
             //Intent TurnOn = new Intent(BluetoothAdapter.ACTION_);
@@ -185,6 +238,87 @@ public class ConnectFragment extends Fragment{
             Toast.makeText(getActivity(), "Bluetooth is now off!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public List<BluetoothGattService> getSupportedGattServices() {
+        if (mBluetoothGatt == null) return null;
+
+        return mBluetoothGatt.getServices();
+    }
+
+    private void displayGattServices(List<BluetoothGattService> gattServices) {
+        if (gattServices == null) return;
+        String uuid = null;
+        String unknownServiceString = "unknown"; //getResources().
+                //getString(R.string.unknown_service);
+        String unknownCharaString = "unknown"; //getResources().
+                //getString(R.string.unknown_characteristic);
+        ArrayList<HashMap<String, String>> gattServiceData =
+                new ArrayList<HashMap<String, String>>();
+        ArrayList<ArrayList<HashMap<String, String>>> gattCharacteristicData
+                = new ArrayList<ArrayList<HashMap<String, String>>>();
+        mGattCharacteristics =
+                new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
+
+        // Loops through available GATT Services.
+        for (BluetoothGattService gattService : gattServices) {
+            HashMap<String, String> currentServiceData =
+                    new HashMap<String, String>();
+            uuid = gattService.getUuid().toString();
+            currentServiceData.put(
+                    LIST_NAME, SampleGattAttributes.
+                            lookup(uuid, unknownServiceString));
+            currentServiceData.put(LIST_UUID, uuid);
+            gattServiceData.add(currentServiceData);
+
+            ArrayList<HashMap<String, String>> gattCharacteristicGroupData =
+                    new ArrayList<HashMap<String, String>>();
+            List<BluetoothGattCharacteristic> gattCharacteristics =
+                    gattService.getCharacteristics();
+            ArrayList<BluetoothGattCharacteristic> charas =
+                    new ArrayList<BluetoothGattCharacteristic>();
+            // Loops through available Characteristics.
+            for (BluetoothGattCharacteristic gattCharacteristic :
+                    gattCharacteristics) {
+                charas.add(gattCharacteristic);
+                HashMap<String, String> currentCharaData =
+                        new HashMap<String, String>();
+                uuid = gattCharacteristic.getUuid().toString();
+                currentCharaData.put(
+                        LIST_NAME, SampleGattAttributes.lookup(uuid,
+                                unknownCharaString));
+                currentCharaData.put(LIST_UUID, uuid);
+                gattCharacteristicGroupData.add(currentCharaData);
+            }
+            mGattCharacteristics.add(charas);
+            gattCharacteristicData.add(gattCharacteristicGroupData);
+        }
+    }
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (mBluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                //updateConnectionState(R.string.connected);
+                mConnectionState = STATE_CONNECTED;
+                getActivity().invalidateOptionsMenu();
+            } else if (mBluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                //updateConnectionState(R.string.disconnected);
+                mConnectionState = STATE_DISCONNECTED;
+                getActivity().invalidateOptionsMenu();
+                //clearUI();
+            } else if (mBluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+                // Show all the supported services and characteristics button_toggle the
+                // user interface.
+                displayGattServices(getSupportedGattServices());
+            } else if (mBluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
+                //displayData(intent.getStringExtra(EXTRA_DATA));
+                Log.e("", intent.getStringExtra(mBluetoothLeService.EXTRA_DATA));
+            }
+        }
+    };
 
     static class ViewHolder{
         TextView deviceAddress;
@@ -255,5 +389,14 @@ public class ConnectFragment extends Fragment{
 
             return view;
         }
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
     }
 }
